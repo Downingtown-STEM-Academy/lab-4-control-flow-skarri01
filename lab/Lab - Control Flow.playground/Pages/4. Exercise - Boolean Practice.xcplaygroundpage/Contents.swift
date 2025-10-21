f325ce8d2e23e9da7d13b6be34f499ad942fca9d
skarri01 <skarri01@student.dasd.org>
2025-10-21 09:51:06 -0400

    Completed Lab 4

--- lab/Lab - Control Flow.playground/Pages/4. Exercise - Boolean Practice.xcplaygroundpage/Contents.swift
+++ lab/Lab - Control Flow.playground/Pages/4. Exercise - Boolean Practice.xcplaygroundpage/Contents.swift
@@ -14,7 +14,11 @@ let hasFish = true
 let hasPizza = false
 let hasVegan = true
 
-
+if hasFish || hasPizza && hasVegan {
+    print("Let's go!")
+}else{
+    print("Sorry, we'll have to think of somewhere else.")
+}
 /*:
  Imagine you're trying to decide whether or not to go on a walk. You decide that you'll go on a walk if it's not raining or if it's 82 degress or warmer and NOT windy. Create a constant `walkingWeather` that is equal to an expression that evaluates to a boolean indicating whether or not the weather is nice enough for you to go for a walk. Write an if statement that will print "I'm going for a walk!" if the weather is nice. (1 points)
  
@@ -26,18 +30,22 @@ let hasVegan = true
 let temp = 82
 let isRaining = true
 let isWindy = false
-/* Set 2
-let temp = 82
-let isRaining = true
-let isWindy = true
-*/
-/* Set 3
-let temp = 75
-let isRaining = false
-let isWindy = true
-*/
+// Set 2
+//let temp = 82
+//let isRaining = true
+//let isWindy = true
 
-//insert your code here!!
+//Set 3
+//let temp = 75
+//let isRaining = false
+//let isWindy = true
 
-//insert print statements here!!
+
+let walkingWeather = !isRaining || (temp >= 82 && !isWindy)
+    print ("I'm gooing for a walk!")
+
+
+print ("I will go for a walk for the first set of conditions")
+print ("I will not go for a walk for the second set of conditions")
+print ("I will not go for a walk for the third set of conditions")
 //: [Previous](@previous)  |  page 4 of 9  |  [Next](@next)
--- lab/Lab - Control Flow.playground/Pages/9. App Exercise - Ternary Messages.xcplaygroundpage/Contents.swift
+++ lab/Lab - Control Flow.playground/Pages/9. App Exercise - Ternary Messages.xcplaygroundpage/Contents.swift
@@ -6,12 +6,15 @@
 let totalQuestions: Int = 20
 let correctQuestions: Int = 18
 
+/*
 if Double(correctQuestions) / Double(totalQuestions) < 0.70 {
     print("You need to study more!")
 } else {
     print("You're doing great.")
 }
-
-
-
+ */
+var status: String = ""
+//Double(correctQuestions)/Double(totalQuestions) < 0.70 ? print("You need to study more") : print("You're doing great.")
+status = Double(correctQuestions)/Double(totalQuestions) < 0.70 ? "You need to study more" : "You're doing great."
+print(status)
 //: [Previous](@previous)  |  page 9 of 9
